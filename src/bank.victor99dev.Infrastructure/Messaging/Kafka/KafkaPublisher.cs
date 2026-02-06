835ac1e5114eaf2a52bb447588c9a1d02ef9c11a
victor99dev <victormotabatista@gmail.com>
2026-02-06 08:18:50 -0300

    configuring the kafka producer

--- src/bank.victor99dev.Infrastructure/Messaging/Kafka/KafkaPublisher.cs
+++ src/bank.victor99dev.Infrastructure/Messaging/Kafka/KafkaPublisher.cs
@@ -3,34 +3,31 @@ using bank.victor99dev.Infrastructure.Configurations;
 using Confluent.Kafka;
 using Microsoft.Extensions.Options;
 
-namespace bank.victor99dev.Infrastructure.Messaging.Kafka
+namespace bank.victor99dev.Infrastructure.Messaging.Kafka;
+
+public class KafkaPublisher : IEventBusPublisher, IDisposable
 {
-    public class KafkaPublisher : IEventBusPublisher, IDisposable
+    private readonly IProducer<string, string> _producer;
+    public KafkaPublisher(IOptions<KafkaConfigurationOptions> kafkaConfigurationOptions)
     {
-        private readonly IProducer<string, string> _producer;
-        private readonly KafkaConfigurationOptions _kafkaConfigurationOptions;
+        var opt = kafkaConfigurationOptions.Value;
 
-        public KafkaPublisher(IOptions<KafkaConfigurationOptions> kafkaConfigurationOptions)
+        var config = new ProducerConfig
         {
-            var opt = kafkaConfigurationOptions.Value;
+            BootstrapServers = opt.BootstrapServers,
+            ClientId = opt.ClientId,
+            Acks = Acks.All,
+            EnableIdempotence = true
+        };
 
-            var config = new ProducerConfig
-            {
-                BootstrapServers = opt.BootstrapServers,
-                ClientId = opt.ClientId,
-                Acks = Acks.All,
-                EnableIdempotence = true
-            };
-
-            _producer = new ProducerBuilder<string, string>(config).Build();
-        }
-        public async Task PublishAsync(string topic, string key, string message, CancellationToken cancellationToken)
-        {
-            var msg = new Message<string, string> { Key = key, Value = message };
-
-            await _producer.ProduceAsync(topic, msg, cancellationToken);
-        }
-
-        public void Dispose() => _producer.Dispose();
+        _producer = new ProducerBuilder<string, string>(config).Build();
     }
+    public async Task PublishAsync(string topic, string key, string message, CancellationToken cancellationToken)
+    {
+        var msg = new Message<string, string> { Key = key, Value = message };
+
+        await _producer.ProduceAsync(topic, msg, cancellationToken);
+    }
+
+    public void Dispose() => _producer.Dispose();
 }
