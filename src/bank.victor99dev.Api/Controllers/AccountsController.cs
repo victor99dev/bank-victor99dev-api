ba0354f3603939244dc413e7b5a317c5a9f12593
victor99dev <victormotabatista@gmail.com>
2026-02-05 23:23:37 -0300

    feat: implementing search by id

--- src/bank.victor99dev.Api/Controllers/AccountsController.cs
+++ src/bank.victor99dev.Api/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
 using bank.victor99dev.Application.UseCases.Accounts.CreateAccount;
+using bank.victor99dev.Application.UseCases.Accounts.GetAccountById;
 using bank.victor99dev.Domain.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,33 +11,59 @@ public class AccountsController : BaseApiController
 {
     private readonly ILogger<AccountsController> _logger;
     private readonly ICreateAccountUseCase _createAccountUseCase;
+    private readonly IGetAccountByIdUseCase _getAccountByIdUseCase;
     public AccountsController(
-        ILogger<AccountsController> logger, 
-        ICreateAccountUseCase createAccountUseCase)
+        ILogger<AccountsController> logger,
+        ICreateAccountUseCase createAccountUseCase,
+        IGetAccountByIdUseCase getAccountByIdUseCase)
     {
         _logger = logger;
         _createAccountUseCase = createAccountUseCase;
+        _getAccountByIdUseCase = getAccountByIdUseCase;
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateCondominium([FromBody] CreateAccountRequest request, CancellationToken cancellationToken)
+    public async Task<IActionResult> CreateAccount([FromBody] CreateAccountRequest request, CancellationToken cancellationToken)
     {
         try
         {
             var result = await _createAccountUseCase.ExecuteAsync(request, cancellationToken);
             
-            LogResult(_logger, result, nameof(CreateCondominium));
+            LogResult(_logger, result, nameof(CreateAccount));
             
             return FromResult(result);
         }
         catch (DomainException ex)
         {
-            LogDomain(_logger, ex, nameof(CreateCondominium));
+            LogDomain(_logger, ex, nameof(CreateAccount));
             return DomainProblem(ex);
         }
         catch (Exception ex)
         {
-            LogUnexpected(_logger, ex, nameof(CreateCondominium));
+            LogUnexpected(_logger, ex, nameof(CreateAccount));
+            return UnexpectedProblem();
+        }
+    }
+
+    
+    [HttpGet("{accountId}")]
+    public async Task<IActionResult> GetAccountById([FromRoute] Guid accountId, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await _getAccountByIdUseCase.ExecuteAsync(accountId, cancellationToken);
+
+            LogResult(_logger, result, nameof(GetAccountById));
+            return FromResult(result);
+        }
+        catch (DomainException ex)
+        {
+            LogDomain(_logger, ex, nameof(GetAccountById));
+            return DomainProblem(ex);
+        }
+        catch (Exception ex)
+        {
+            LogUnexpected(_logger, ex, nameof(GetAccountById));
             return UnexpectedProblem();
         }
     }
--- src/bank.victor99dev.Application/UseCases/Accounts/GetAccountById/GetAccountByIdUseCase.cs
+++ src/bank.victor99dev.Application/UseCases/Accounts/GetAccountById/GetAccountByIdUseCase.cs
@@ -1,3 +1,4 @@
+using bank.victor99dev.Application.Interfaces.Repository;
 using bank.victor99dev.Application.Shared.Results;
 using bank.victor99dev.Application.UseCases.Accounts.Shared;
 
@@ -5,8 +6,28 @@ namespace bank.victor99dev.Application.UseCases.Accounts.GetAccountById;
 
 public class GetAccountByIdUseCase : IGetAccountByIdUseCase
 {
-    public Task<Result<AccountResponse>> ExecuteAsync(Guid accountId, CancellationToken cancellationToken = default)
+    private readonly IAccountRepository _accountRepository;
+    public GetAccountByIdUseCase(IAccountRepository accountRepository)
     {
-        throw new NotImplementedException();
+        _accountRepository = accountRepository;
+    }
+
+    public async Task<Result<AccountResponse>> ExecuteAsync(Guid accountId, CancellationToken cancellationToken = default)
+    {
+        var account = await _accountRepository.GetByIdAsync(accountId, cancellationToken);
+        if (account is null)
+            return Result.Fail<AccountResponse>($"The account id {accountId} was not found.", ResultStatus.NotFound);
+
+        var response = new AccountResponse
+        {
+            Id = account.Id,
+            Name = account.AccountName.Value,
+            Cpf = account.Cpf.Value,
+            IsActive = account.IsActive,
+            IsDeleted = account.IsDeleted,
+            CreatedAt = account.CreatedAt
+        };
+
+        return Result<AccountResponse>.Ok(data: response);
     }
 }
